d01c0a4aedf84b28d6d06ec61519eccf012d6263
alex-norris <anor013@gmail.com>
2022-12-03 13:21:44 -0500

    Update 3a.py

--- 3a.py
+++ 3a.py
@@ -1,1 +1,19 @@
 
+with open('3.txt', 'r') as f:
+    lines = [line.rstrip('\n') for line in f]
+
+prioritySum = 0
+for i in lines:
+    #print(i)
+    a = i[0:(int((len(i)/2)))]
+    b = i[int((len(i)/2)):]
+    for j in a:
+        if j in b:
+            if (ord(j)-96) > 0:
+                prioritySum += ord(j)-96 # ascii to one-based
+                #print(j,ord(j)-96)
+            else:
+                prioritySum += ord(j)-38 # ascii to 27 based
+                #print(j,ord(j)-38)
+            break
+print(prioritySum)
