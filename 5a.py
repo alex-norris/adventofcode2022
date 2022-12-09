b3a88ac2ddc41f24d3c952b61dd3d10be75cd1b3
alex-norris <anor013@gmail.com>
2022-12-08 19:13:46 -0500

    Create 5a.py

---
+++ 5a.py
@@ -0,0 +1,21 @@
+with open('5.txt', 'r') as f:
+    lines = [line.rstrip('\n') for line in f]
+
+rows, cols = (9, 1)
+stacks = [[0 for i in range(cols)] for j in range(rows)]
+
+for i in range(7,-1,-1):
+    for j in range(1,len(lines[i]),4):
+        if lines[i][j] != ' ':
+            stacks[int((j-1)/4)].append(lines[i][j])
+
+for i in range(10,len(lines)):
+    move = int(lines[i].split(' ')[1])
+    start = int(lines[i].split(' ')[3])-1
+    end = int(lines[i].split(' ')[5])-1
+
+    for j in range(move):
+        stacks[end].append(stacks[start].pop())
+
+for i in range(9):
+    print(stacks[i][-1], end = '')
