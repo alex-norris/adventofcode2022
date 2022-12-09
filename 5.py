1f8a95fad041da16479ff0e41deeee4c7f9600ae
alex-norris <anor013@gmail.com>
2022-12-08 20:04:47 -0500

    Update and rename 5a.py to 5.py

--- 5.py
+++ 5.py
@@ -6,8 +6,13 @@ stacks = [[0 for i in range(cols)] for j in range(rows)]
 
 for i in range(7,-1,-1):
     for j in range(1,len(lines[i]),4):
+        # for 5a:
         if lines[i][j] != ' ':
             stacks[int((j-1)/4)].append(lines[i][j])
+        # for 5b:
+        #for j in range(-move,0):
+            #stacks[end].append(stacks[start][j])
+            #stacks[start].pop(j)
 
 for i in range(10,len(lines)):
     move = int(lines[i].split(' ')[1])
